886c0cd8b6b361cd8b6992a9ee70d90cb8f3910d
Anatoli <anatoli@anatoli.home>
2023-02-07 20:18:04 +0300

    Weekend Database Schema

---
+++ Database/weekend_ETS.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 07, 2023 at 05:59 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `weekend_ETS`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(11) NOT NULL,
+  `category_init_expense` varchar(200) NOT NULL,
+  `category_status` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `expense_id` int(200) NOT NULL,
+  `expense_category_id` int(11) NOT NULL,
+  `expense_fm_id` int(11) NOT NULL,
+  `expense_user_id` int(11) NOT NULL,
+  `expense_cost` varchar(200) DEFAULT NULL,
+  `expense_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fiscal_month`
+--
+
+CREATE TABLE `fiscal_month` (
+  `fm_id` int(11) NOT NULL,
+  `fm_year` varchar(200) NOT NULL,
+  `fm_month` varchar(200) NOT NULL,
+  `salary` varchar(200) DEFAULT NULL,
+  `fm_status` varchar(200) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mailer_settings`
+--
+
+CREATE TABLE `mailer_settings` (
+  `id` int(20) NOT NULL,
+  `mailer_host` varchar(200) DEFAULT NULL,
+  `mailer_port` varchar(200) DEFAULT NULL,
+  `mailer_protocol` varchar(200) DEFAULT NULL,
+  `mailer_username` varchar(200) DEFAULT NULL,
+  `mailer_mail_from_name` varchar(200) DEFAULT NULL,
+  `mailer_mail_from_email` varchar(200) DEFAULT NULL,
+  `mailer_password` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_email` varchar(200) NOT NULL,
+  `user_password` varchar(200) NOT NULL,
+  `user_reset_token` longtext DEFAULT NULL,
+  `user_first_name` varchar(200) DEFAULT NULL,
+  `user_last_name` varchar(200) DEFAULT NULL,
+  `user_current_salary` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`expense_id`),
+  ADD KEY `Expensetouser` (`expense_user_id`),
+  ADD KEY `Expensetofiscalyear` (`expense_fm_id`),
+  ADD KEY `Expensetocategory` (`expense_category_id`);
+
+--
+-- Indexes for table `fiscal_month`
+--
+ALTER TABLE `fiscal_month`
+  ADD PRIMARY KEY (`fm_id`);
+
+--
+-- Indexes for table `mailer_settings`
+--
+ALTER TABLE `mailer_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `expense_id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `fiscal_month`
+--
+ALTER TABLE `fiscal_month`
+  MODIFY `fm_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mailer_settings`
+--
+ALTER TABLE `mailer_settings`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD CONSTRAINT `Expensetocategory` FOREIGN KEY (`expense_category_id`) REFERENCES `categories` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Expensetofiscalyear` FOREIGN KEY (`expense_fm_id`) REFERENCES `fiscal_month` (`fm_id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Expensetouser` FOREIGN KEY (`expense_user_id`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
